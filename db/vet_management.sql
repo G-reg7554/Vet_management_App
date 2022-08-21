1987f4112d31767d70da2b3295edc3b35590fcc9
Gregor Campbell <gregorc93@hotmail.co.uk>
2022-08-21 18:39:07 +0100

    added tables for vets and pets

---
+++ db/vet_management.sql
@@ -0,0 +1,16 @@
+DROP TABLE IF EXISTS pets;
+DROP TABLE IF EXISTS vets;
+
+CREATE TABLE vets (
+    id SERIAL PRIMARY KEY,
+    full_name VARCHAR(255)
+);
+
+CREATE TABLE pets (
+    id SERIAL PRIMARY KEY,
+    pets_name VARCHAR(255),
+    date_of_birth VARCHAR(255),
+    pet_type VARCHAR(255),
+    contact_number VARCHAR(255),
+    treatment_notes VARCHAR(255)
+);
