bf0a5bded4b417ed38b3543372f2ad76fd8be35a
Gregor Campbell <gregorc93@hotmail.co.uk>
2022-08-23 01:12:53 +0100

    added several pages listing my vets and pets from the database

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
 from controllers.pet_controller import pet_blueprint
 
 app = Flask(__name__)
--- controllers/pet_controller.py
+++ controllers/pet_controller.py
@@ -3,11 +3,17 @@ from flask import Blueprint
 import repositories.pet_repository as pet_repository
 import repositories.vet_repository as vet_repository
 
-pet_blueprint = Blueprint("tasks", __name__)
+pet_blueprint = Blueprint("pet", __name__)
 
-@pet_blueprint.route("/tasks")
-def tasks():
+@pet_blueprint.route("/pet")
+def pet_list():
     pets = pet_repository.select_all()
     vets = vet_repository.select_all()
-    return render_template("new.html", vet_staff = vets, pet_patient = pets)
+    return render_template("pet_list.html", vet_staff = vets, pet_patient = pets)
 
+
+@pet_blueprint.route("/vet")
+def vet_list():
+    pets = pet_repository.select_all()
+    vets = vet_repository.select_all()
+    return render_template("vet_list.html", vet_staff = vets, pet_patient = pets)
